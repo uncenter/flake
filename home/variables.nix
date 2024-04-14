7c0048bc633dfc959d7fd209d5fd61c4f548f34e
uncenter <47499684+uncenter@users.noreply.github.com>
2024-04-13 23:41:10 -0400

    chore: remove unneeded env vars

--- home/variables.nix
+++ home/variables.nix
@@ -1,10 +1,6 @@
 {config, ...}: {
   home.sessionVariables = {
     # https://github.com/b3nj5m1n/xdg-ninja
-    ZDOTDIR = "${config.xdg.configHome}/zsh";
-    GIT_CONFIG_GLOBAL = "${config.xdg.configHome}/git/config";
-    PYTHONSTARTUP = "${config.xdg.configHome}/python/pythonrc.py";
-    PYENV_ROOT = "${config.xdg.dataHome}/pyenv";
     NPM_CONFIG_USERCONFIG = "${config.xdg.configHome}/npm/npmrc";
     NODE_REPL_HISTORY = "${config.xdg.dataHome}/nodejs/history";
     PNPM_HOME = "${config.xdg.cacheHome}/pnpm";
@@ -16,10 +12,7 @@
     YARN_CACHE_FOLDER = "${config.xdg.cacheHome}/yarn";
     YARN_ENABLE_TELEMETRY = "0";
     GNUPGHOME = "${config.xdg.configHome}/gnupg";
-    TEALDEER_CONFIG_DIR = "${config.xdg.configHome}/tealdeer";
-    TEALDEER_CACHE_DIR = "${config.xdg.cacheHome}/tealdeer";
     GOPATH = "${config.xdg.dataHome}/go";
-    STACK_XDG = "1";
     WAKATIME_HOME = "${config.xdg.configHome}/wakatime";
     BUNDLE_USER_CONFIG = "${config.xdg.configHome}/bundle";
     BUNDLE_USER_CACHE = "${config.xdg.cacheHome}/bundle";
