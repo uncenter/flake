7e6252f110c2754e9aeb448a77462d2dbb7e2f6a
uncenter <47499684+uncenter@users.noreply.github.com>
2024-04-22 20:01:22 -0400

    feat(scripts): add wifi-password

--- home/scripts.nix
+++ home/scripts.nix
@@ -9,16 +9,25 @@
       "${config.home.homeDirectory}/.local/bin"
     ];
 
-    file = builtins.listToAttrs (builtins.map (name: {
-      name = ".local/bin/${lib.removeSuffix ".sh" (builtins.baseNameOf name)}";
-      value = {
-        source = lib.getExe (pkgs.writeShellApplication {
-          name = builtins.baseNameOf name;
-          text = builtins.readFile ./scripts/${name};
+    file =
+      builtins.listToAttrs (builtins.map (name: {
+        name = ".local/bin/${lib.removeSuffix ".sh" (builtins.baseNameOf name)}";
+        value = {
+          source = lib.getExe (pkgs.writeShellApplication {
+            name = builtins.baseNameOf name;
+            text = builtins.readFile ./scripts/${name};
 
-          bashOptions = ["errexit" "pipefail"];
-        });
+            bashOptions = ["errexit" "pipefail"];
+          });
+        };
+      }) (builtins.attrNames (builtins.readDir ./scripts)))
+      // {
+        ".local/bin/wifi-password".source = "${pkgs.fetchFromGitHub {
+          owner = "uncenter";
+          repo = "wifi-password";
+          rev = "f4251aa27c18c6f9f96a30d1e6e5f6d759833242";
+          sha256 = "sha256-+gefSWagzydDMQhuG26/iNam+4JNAcpB3qqZDh/TjEs=";
+        }}/wifi-password.sh";
       };
-    }) (builtins.attrNames (builtins.readDir ./scripts)));
   };
 }
