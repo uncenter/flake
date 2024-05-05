9fc48863801a6a0ba244723b78bfb1793a265321
uncenter <47499684+uncenter@users.noreply.github.com>
2024-05-05 13:16:59 -0400

    refactor(find-prs): split into vars

--- find-prs.sh
+++ find-prs.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-gh pr list --repo nixos/nixpkgs --draft=false --json "title,number" | jq ".[] | select(.title | $(nix eval --json '.#darwinConfigurations.Katara.config.environment.systemPackages' | jq -r '.[] | capture("/nix/store/[A-Za-z0-9]+-(?<package>[A-Za-z-]+)(?![0-9])") | .package' | awk '{printf "%scontains(\"%s:\")", (NR>1 ? " or " : ""), $0} END{print ""}'))"
+REPOSITORY="nixos/nixpkgs"
+CONFIGURATION="darwinConfigurations.Katara"
+
+gh pr list --repo "$REPOSITORY" --draft=false --json "title,number" | jq ".[] | select(.title | $(nix eval --json ".#$CONFIGURATION.config.environment.systemPackages" | jq -r '.[] | capture("/nix/store/[A-Za-z0-9]+-(?<package>[A-Za-z-]+)(?![0-9])") | .package' | awk '{printf "%scontains(\"%s:\")", (NR>1 ? " or " : ""), $0} END{print ""}'))"
