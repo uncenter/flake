bc1c861ba01d5e54929b6b3de48ffb0fe430c140
uncenter <47499684+uncenter@users.noreply.github.com>
2024-05-05 16:41:41 -0400

    refactor(find-prs): no longer one line!

--- find-prs.sh
+++ find-prs.sh
@@ -3,4 +3,8 @@
 REPOSITORY="nixos/nixpkgs"
 CONFIGURATION="darwinConfigurations.Katara"
 
-gh pr list --repo "$REPOSITORY" --draft=false --json "title,number" | jq ".[] | select(.title | $(nix eval --json ".#$CONFIGURATION.config.environment.systemPackages"  --apply "map (pkg: (builtins.parseDrvName pkg.name).name)" |  jq -r '.[] | "startswith(\"\(.):\")"' | tr '\n' '@' | sed 's/@/ or /g' | rev | cut -c 4- | rev))" | jq -r '"\(.title) - https://github.com/nixos/nixpkgs/pull/\(.number)"'
+prs=$(gh pr list --repo "$REPOSITORY" --draft=false --json "title,number")
+packages=$(nix eval --json ".#$CONFIGURATION.config.environment.systemPackages"  --apply "map (pkg: (builtins.parseDrvName pkg.name).name)")
+filters=$(echo "$packages" |  jq -r '.[] | "startswith(\"\(.):\")"' | tr '\n' '@' | sed 's/@/ or /g' | rev | cut -c 4- | rev)
+
+echo "$prs" | jq ".[] | select(.title | $filters)" | jq -r "\"\(.title) - https://github.com/$REPOSITORY/pull/\(.number)\""
