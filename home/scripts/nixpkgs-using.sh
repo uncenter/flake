a03af5c27315d4dae3031720af5700669f77669d
uncenter <47499684+uncenter@users.noreply.github.com>
2024-05-06 17:15:49 -0400

    fix(scripts/nixpkgs-using): no limit on gh api, filter for home-manager packages too

--- home/scripts/nixpkgs-using.sh
+++ home/scripts/nixpkgs-using.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-REPOSITORY="nixos/nixpkgs"
+FLAKE="/Users/uncenter/.config/flake"
 CONFIGURATION="darwinConfigurations.Katara"
+USERNAME="uncenter"
+REPOSITORY="nixos/nixpkgs"
 
-prs=$(gh pr list --repo "$REPOSITORY" --draft=false --json "title,number")
-packages=$(nix eval --json ".#$CONFIGURATION.config.environment.systemPackages"  --apply "map (pkg: (builtins.parseDrvName pkg.name).name)")
+prs=$(gh pr list --repo "$REPOSITORY" --draft=false --limit 5000 --json "title,number")
+packages=$(nix eval --impure --json --expr "(builtins.getFlake \"$FLAKE\").$CONFIGURATION.config.home-manager.users.$USERNAME.home.packages ++ (builtins.getFlake \"$FLAKE\").$CONFIGURATION.config.environment.systemPackages"  --apply "map (pkg: (builtins.parseDrvName pkg.name).name)")
 filters=$(echo "$packages" |  jq -r '.[] | "startswith(\"\(.):\")"' | tr '\n' '@' | sed 's/@/ or /g' | rev | cut -c 4- | rev)
 
 echo "$prs" | jq ".[] | select(.title | $filters)" | jq -r "\"\(.title) - https://github.com/$REPOSITORY/pull/\(.number)\""
