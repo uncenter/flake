da559ba5af0be005a6b817d0b7c3e91d46be3dc3
uncenter <47499684+uncenter@users.noreply.github.com>
2024-05-07 10:12:24 -0400

    feat(nix): add substituters

--- modules/nix.nix
+++ modules/nix.nix
@@ -26,6 +26,24 @@
     sandbox = true;
     use-xdg-base-directories = true;
 
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+      "https://nixpkgs-unfree.cachix.org"
+      "https://numtide.cachix.org"
+      "https://catppuccin.cachix.org"
+      "https://cache.garnix.io"
+    ];
+
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+
     nix-path = config.nix.nixPath;
   };
 
