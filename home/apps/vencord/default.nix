e5d09f4944ae59eb1781fba8c53b8c582d1ed309
uncenter <47499684+uncenter@users.noreply.github.com>
2024-06-17 16:10:45 -0400

    fix(apps/vencord): symlinks

--- home/apps/vencord/default.nix
+++ home/apps/vencord/default.nix
@@ -3,12 +3,12 @@
 
   flakeDir = "/Users/uncenter/.config/flake/home/apps/vencord";
   settingsFile = mkLink "${flakeDir}/settings.json";
-  cssFile = mkLink "${flakeDir}/quickCss.json";
+  cssFile = mkLink "${flakeDir}/quickCss.css";
 in {
   home.file = let
     common = "Library/Application Support/Vencord/settings";
   in {
-    "${common}/settings.json".source = ./settings.json;
-    "${common}/quickCss.css".source = ./quickCss.css;
+    "${common}/settings.json".source = settingsFile;
+    "${common}/quickCss.css".source = cssFile;
   };
 }
