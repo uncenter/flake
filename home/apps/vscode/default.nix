778f6835e6a3176dce323e0412f0925383efbc15
uncenter <47499684+uncenter@users.noreply.github.com>
2024-06-18 12:04:20 -0400

    refactor(apps/vscode): organize extensions, remove unneeded

--- home/apps/vscode/default.nix
+++ home/apps/vscode/default.nix
@@ -12,8 +12,12 @@ in {
     package = pkgs.vscode;
 
     extensions = with pkgs.vscode-extensions; [
-      astro-build.astro-vscode
-      bierner.comment-tagged-templates
+      # Appearance
+      catppuccin.catppuccin-vsc
+      catppuccin.catppuccin-vsc-icons
+      zguolee.tabler-icons
+
+      # Markdown
       bierner.emojisense
       bierner.github-markdown-preview
       bierner.markdown-checkbox
@@ -21,57 +25,55 @@ in {
       bierner.markdown-footnotes
       bierner.markdown-mermaid
       bierner.markdown-preview-github-styles
-      bierner.markdown-yaml-preamble
+      yzhang.markdown-all-in-one
+      yahyabatulu.vscode-markdown-alert
+
+      # Language/Tool Support
+      astro-build.astro-vscode
       biomejs.biome
       bmalehorn.vscode-fish
       bradlc.vscode-tailwindcss
-      catppuccin.catppuccin-vsc
-      catppuccin.catppuccin-vsc-icons
       charliermarsh.ruff
       dbaeumer.vscode-eslint
       denoland.vscode-deno
       eamodio.gitlens
       editorconfig.editorconfig
       esbenp.prettier-vscode
-      fabiospampinato.vscode-open-in-github
       fortran-lang.linter-gfortran
-      ginfuru.better-nunjucks
       github.vscode-github-actions
       golang.go
       graphql.vscode-graphql-syntax
       griimick.vhs
-      hyoban.tailwindcss-classname-highlight
       jnoortheen.nix-ide
-      johnnymorganz.stylua
-      kamadorueda.alejandra
-      kravets.vscode-publint
-      leonardssh.vscord
       mechatroner.rainbow-csv
-      meganrogge.template-string-converter
       mrmlnc.vscode-scss
       ms-python.debugpy
       ms-python.python
-      ms-vscode-remote.remote-wsl
       ms-vscode.cpptools
-      njpwerner.autodocstring
       oven.bun-vscode
       redhat.vscode-yaml
       rust-lang.rust-analyzer
-      sclu1034.justfile
+      nefrob.vscode-just-syntax
       svelte.svelte-vscode
       tamasfe.even-better-toml
       timonwong.shellcheck
       unifiedjs.vscode-mdx
-      usernamehw.errorlens
       visualstudioexptteam.intellicode-api-usage-examples
       visualstudioexptteam.vscodeintellicode
-      vitaliymaz.vscode-svg-previewer
       vue.volar
-      wakatime.vscode-wakatime
-      yahyabatulu.vscode-markdown-alert
       yoavbls.pretty-ts-errors
-      yzhang.markdown-all-in-one
-      zguolee.tabler-icons
+
+      # Utilities
+      bierner.comment-tagged-templates
+      wakatime.vscode-wakatime
+      vitaliymaz.vscode-svg-previewer
+      usernamehw.errorlens
+      meganrogge.template-string-converter
+      fabiospampinato.vscode-open-in-github
+      kravets.vscode-publint
+      leonardssh.vscord
+      njpwerner.autodocstring
+      ms-vscode-remote.remote-wsl
     ];
     mutableExtensionsDir = true;
   };
