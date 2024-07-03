8b9f491ba8c4fcf4f4ff690cf044f817be7e88c7
uncenter <47499684+uncenter@users.noreply.github.com>
2024-07-03 18:00:50 -0400

    feat(apps/ghostty): nixify config file with `generators.toINI`, add new splits keybinds

--- home/apps/ghostty/default.nix
+++ home/apps/ghostty/default.nix
@@ -1,3 +1,36 @@
-{config, ...}: {
-  home.file."${config.xdg.configHome}/ghostty/config".source = ./ghostty.conf;
+{
+  config,
+  lib,
+  ...
+}: {
+  home.file."${config.xdg.configHome}/ghostty/config".text =
+    lib.generators.toINIWithGlobalSection {
+      listsAsDuplicateKeys = true;
+    } {
+      globalSection = {
+        # https://github.com/mitchellh/ghostty/blob/main/src/config/Config.zig
+
+        theme = "catppuccin-${config.catppuccin.flavor}";
+        font-family = "CommitMono Nerd Font";
+        font-size = 15;
+        adjust-cell-height = 10;
+        window-padding-x = 25;
+        window-padding-y = 25;
+
+        macos-titlebar-style = "tabs";
+
+        working-directory = "/Users/uncenter/Dev";
+        window-inherit-working-directory = false;
+
+        copy-on-select = false;
+        shell-integration = "detect";
+        macos-option-as-alt = true;
+
+        keybind = [
+          "super+backspace=text:\x15"
+          "cmd+shift+d=new_split:down"
+          "cmd+shift+r=new_split:right"
+        ];
+      };
+    };
 }
