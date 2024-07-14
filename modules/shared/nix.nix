fadde6c4bbe710b2429f66dc09df6f33c46d85d0
uncenter <47499684+uncenter@users.noreply.github.com>
2024-07-14 17:54:51 -0400

    feat(modules/shared): add personal cachix cache substituter and public key

--- modules/shared/nix.nix
+++ modules/shared/nix.nix
@@ -28,6 +28,8 @@
       use-xdg-base-directories = true;
 
       substituters = [
+        "https://uncenter.cachix.org"
+
         "https://cache.nixos.org/"
         "https://nix-community.cachix.org"
         "https://nixpkgs-unfree.cachix.org"
@@ -37,6 +39,8 @@
       ];
 
       trusted-public-keys = [
+        "uncenter.cachix.org-1:oTEVc2NvAzhiZgE6QiY81ASQDXTAH7roX9i8zA7ub2g="
+
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
