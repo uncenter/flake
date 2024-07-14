bcb83e47e827c7554e101533c29cecf53e2d8b73
uncenter <47499684+uncenter@users.noreply.github.com>
2024-07-14 16:56:45 -0400

    fix(systems/katara): update hostname

--- systems/darwin.nix
+++ systems/darwin.nix
@@ -2,7 +2,7 @@
 {
   flake = {
     darwinConfigurations = {
-      Katara = inputs.nix-darwin.lib.darwinSystem {
+      katara = inputs.nix-darwin.lib.darwinSystem {
         modules = [
           inputs.home-manager.darwinModules.home-manager
           inputs.darwin-custom-icons.darwinModules.default
--- systems/katara/default.nix
+++ systems/katara/default.nix
@@ -2,5 +2,11 @@
 {
   imports = [ ./icons.nix ];
 
+  networking = {
+    computerName = "Katara";
+    hostName = "katara";
+    localHostName = "katara";
+  };
+
   security.pam.enableSudoTouchIdAuth = true;
 }
