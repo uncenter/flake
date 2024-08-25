1a0998f5ae5608faf1833ee13f13555ebcd0207c
uncenter <47499684+uncenter@users.noreply.github.com>
2024-08-25 11:04:50 -0400

    chore(systems/katara): update used icons

--- systems/katara/icons.nix
+++ systems/katara/icons.nix
@@ -8,28 +8,12 @@
         icon = ./icons/1password.icns;
       }
       {
-        path = "/Applications/Arc.app";
-        icon = ./icons/arc.icns;
-      }
-      {
         path = "/Applications/Calibre.app";
         icon = ./icons/calibre.icns;
       }
       {
         path = "/Applications/DevFolderIcon.app";
         icon = ./icons/devfolder.icns;
-      }
-      {
-        path = "/Applications/Discord.app";
-        icon = ./icons/discord.icns;
-      }
-      {
-        path = "/Applications/DevFolderIcon.app";
-        icon = ./icons/devfolder.icns;
-      }
-      {
-        path = "/Applications/Figma.app/";
-        icon = ./icons/figma.icns;
       }
       {
         path = "/Applications/Ghostty.app";
@@ -40,36 +24,12 @@
         icon = ./icons/gramps.icns;
       }
       {
-        path = "/Applications/Inkscape.app";
-        icon = ./icons/inkscape.icns;
-      }
-      {
-        path = "/Applications/Obsidian.app";
-        icon = ./icons/obsidian.icns;
-      }
-      {
         path = "/Applications/ScreenshotsFolderIcon.app";
         icon = ./icons/screenshotsfolder.icns;
-      }
-      {
-        path = "/Applications/ScreenshotsFolderIcon.app";
-        icon = ./icons/screenshotsfolder.icns;
-      }
-      {
-        path = "/Applications/Spotify.app";
-        icon = ./icons/spotify.icns;
-      }
-      {
-        path = "/Applications/Visual Studio Code.app";
-        icon = ./icons/vscode.icns;
       }
       {
         path = "/Applications/zoom.us.app";
         icon = ./icons/zoom.icns;
-      }
-      {
-        path = "/Applications/Zed.app";
-        icon = ./icons/zed.icns;
       }
     ];
   };
