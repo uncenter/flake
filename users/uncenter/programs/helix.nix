8a8ebf1a0f04e07b18227aa77501863ba82a4416
uncenter <47499684+uncenter@users.noreply.github.com>
2024-09-06 23:17:06 -0400

    fix(programs/helix): render whitespace characters, auto format nix code

--- users/uncenter/programs/helix.nix
+++ users/uncenter/programs/helix.nix
@@ -1,7 +1,26 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   programs.helix = {
     enable = true;
     catppuccin.enable = true;
+
+    settings = {
+      editor.whitespace.render = {
+        space = "all";
+        tab = "all";
+      };
+    };
+
+    languages = {
+      language = [
+        {
+          name = "nix";
+          formatter = {
+            command = lib.getExe pkgs.nixfmt-rfc-style;
+          };
+          auto-format = true;
+        }
+      ];
+    };
   };
 }
