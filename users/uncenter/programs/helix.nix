fdeb9e37148b307ef787c74f1010a9ecc6ccb0b6
uncenter <47499684+uncenter@users.noreply.github.com>
2024-09-08 13:22:29 -0400

    feat(programs/helix): add deno, ruff, and fish formatting/lsp

--- users/uncenter/programs/helix.nix
+++ users/uncenter/programs/helix.nix
@@ -15,15 +15,98 @@
     };
 
     languages = {
-      language = [
-        {
-          name = "nix";
-          formatter = {
-            command = lib.getExe pkgs.nixfmt-rfc-style;
+      language =
+        let
+          lang-to-ext = {
+            javascript = "js";
+            json = "json";
+            markdown = "md";
+            typescript = "ts";
+            jsx = "jsx";
+            tsx = "tsx";
           };
+        in
+        [
+          {
+            name = "nix";
+            formatter = {
+              command = lib.getExe pkgs.nixfmt-rfc-style;
+            };
+            auto-format = true;
+          }
+          {
+            name = "python";
+            formatter = {
+              command = "ruff";
+              args = [
+                "format"
+                "-"
+              ];
+            };
+            auto-format = true;
+          }
+          {
+            name = "fish";
+            formatter = {
+              command = "fish_indent";
+            };
+            auto-format = true;
+          }
+        ]
+        ++ (lib.attrsets.mapAttrsToList (lang: ext: {
           auto-format = true;
-        }
-      ];
+          name = lang;
+          roots = [
+            "deno.json"
+            "deno.jsonc"
+          ];
+          formatter = {
+            args = [
+              "fmt"
+              "-"
+              "--ext"
+              ext
+            ];
+            command = "deno";
+          };
+        }) lang-to-ext)
+        ++ (lib.attrsets.mapAttrsToList (lang: ext: {
+          name = lang;
+          shebangs = [
+            "deno"
+          ];
+          roots = [
+            "deno.json"
+            "deno.jsonc"
+          ];
+          file-types = [
+            ext
+          ];
+          language-servers = [
+            "deno-lsp"
+          ];
+          auto-format = true;
+        }) lang-to-ext);
+
+      language-server = {
+        deno-lsp = {
+          command = "deno";
+          args = [ "lsp" ];
+          environment.NO_COLOR = "1";
+          config.deno = {
+            enable = true;
+            cacheOnSave = true;
+            suggest = {
+              completeFunctionCalls = false;
+              imports = {
+                hosts = {
+                  "https://deno.land" = true;
+                };
+              };
+            };
+          };
+        };
+      };
     };
 
     extraPackages = with pkgs; [ nodePackages.typescript-language-server ];
