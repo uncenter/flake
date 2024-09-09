7365f301766c03751912e17eb43df284419913c5
uncenter <47499684+uncenter@users.noreply.github.com>
2024-09-08 21:26:42 -0400

    fix(programs/yazi): don't sort in reverse

--- users/uncenter/programs/yazi.nix
+++ users/uncenter/programs/yazi.nix
@@ -23,7 +23,7 @@ in
         ];
         sort_by = "natural";
         sort_sensitive = true;
-        sort_reverse = true;
+        sort_reverse = false;
         sort_dir_first = true;
         linemode = "none";
         show_hidden = true;
