033fc91a96a6bebcf5e9f0832a12860a9b791608
uncenter <47499684+uncenter@users.noreply.github.com>
2024-10-09 22:51:44 -0400

    fix(programs/gleam): inotify-tools linux-only

--- users/uncenter/programs/gleam.nix
+++ users/uncenter/programs/gleam.nix
@@ -1,11 +1,14 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    erlang_26
-    gleam
-    rebar3
-
-    # Required for Lustre's dev server.
-    inotify-tools
-  ];
+  home.packages =
+    with pkgs;
+    [
+      erlang_26
+      gleam
+      rebar3
+    ]
+    ++ lib.optionals pkgs.stdenv.isLinux [
+      # Required for Lustre's dev server.
+      inotify-tools
+    ];
 }
