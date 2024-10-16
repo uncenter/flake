ce9d868bc0a2b1c7851811c69d4dd219a08d94ee
uncenter <47499684+uncenter@users.noreply.github.com>
2024-10-16 18:26:46 -0400

    chore: cleanup

--- users/uncenter/programs/mommy.nix
+++ users/uncenter/programs/mommy.nix
@@ -3,6 +3,6 @@
     MOMMY_SWEETIE="leah/girl"
     MOMMY_FORBIDDEN_WORDS="fat"
     MOMMY_SUFFIX="~/ :3/ <3"
-    MOMMY_COMPLIMENTS_EXTRA="you're being such a good girl/you're making mommy so happy/mommy loves you/well done~! mommy is so happy for you"
+    MOMMY_COMPLIMENTS_EXTRA="you're being such a good girl/well done~!"
   '';
 }
