630cebe54b75d6a1739c1010149078aa1083bb19
uncenter <47499684+uncenter@users.noreply.github.com>
2024-12-21 12:49:12 -0500

    fix(apps/zed): use globally set ctp flavor

--- users/uncenter/apps/zed.nix
+++ users/uncenter/apps/zed.nix
@@ -1,47 +1,51 @@
 {
-  pkgs,
   lib,
+  pkgs,
+  config,
   ...
 }:
+let
+  mkUpper =
+    str:
+    (lib.toUpper (builtins.substring 0 1 str)) + (builtins.substring 1 (builtins.stringLength str) str);
+in
 {
-  config = lib.mkIf pkgs.stdenv.isDarwin {
-    programs.zed-editor = {
-      enable = true;
+  programs.zed-editor = lib.mkIf pkgs.stdenv.isDarwin {
+    enable = true;
 
-      extensions = [
-        # Appearance
-        "catppuccin"
+    extensions = [
+      # Appearance
+      "catppuccin"
 
-        # Language/Tool Support
-        "html"
-        "superhtml"
-        "tera"
-        "toml"
-        "vue"
-        "scss"
-        "nix"
-        "just"
-        "scheme"
-      ];
+      # Language/Tool Support
+      "html"
+      "superhtml"
+      "tera"
+      "toml"
+      "vue"
+      "scss"
+      "nix"
+      "just"
+      "scheme"
+    ];
 
-      userSettings = {
-        buffer_font_family = "Lilex Nerd Font";
-        theme = {
-          mode = "system";
-          light = "Catppuccin Latte";
-          dark = "Catppuccin Macchiato";
-        };
-        format_on_save = "on";
-        languages = {
-          Nix = {
-            language_servers = [
-              "nil"
-              "!nixd"
-            ];
-            formatter = {
-              external = {
-                command = "nixfmt";
-              };
+    userSettings = {
+      buffer_font_family = "Lilex Nerd Font";
+      theme = {
+        mode = "system";
+        light = "Catppuccin Latte";
+        dark = "Catppuccin ${mkUpper config.catppuccin.flavor}";
+      };
+      format_on_save = "on";
+      languages = {
+        Nix = {
+          language_servers = [
+            "nil"
+            "!nixd"
+          ];
+          formatter = {
+            external = {
+              command = "nixfmt";
             };
           };
         };
