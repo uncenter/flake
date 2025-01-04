4f8adfd860fabf480bf4c08ab93f57f08e6efdaf
uncenter <47499684+uncenter@users.noreply.github.com>
2025-01-04 16:22:03 -0500

    fix: enable spicetify under `apps` option

--- users/uncenter/programs/spicetify.nix
+++ users/uncenter/programs/spicetify.nix
@@ -12,7 +12,7 @@ in
 {
   imports = [ inputs.spicetify-nix.homeManagerModules.default ];
 
-  programs.spicetify = lib.mkIf osConfig.glade.programs.media.enable {
+  programs.spicetify = lib.mkIf osConfig.glade.apps.enable {
     enable = true;
     theme = spicePkgs.themes.catppuccin;
     colorScheme = config.catppuccin.flavor;
