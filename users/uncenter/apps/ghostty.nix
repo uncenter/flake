d19fc795b05eaded27f09fb9ea8950232dc0935d
uncenter <47499684+uncenter@users.noreply.github.com>
2025-01-25 10:27:06 -0500

    refactor(apps/ghostty): use home-manager module and ctp/nix integration

--- users/uncenter/apps/ghostty.nix
+++ users/uncenter/apps/ghostty.nix
@@ -1,42 +1,54 @@
 {
   lib,
+  pkgs,
   config,
   osConfig,
   ...
 }:
 {
-  xdg.configFile."ghostty/config" = lib.mkIf osConfig.glade.apps.enable {
-    text = lib.generators.toINIWithGlobalSection { listsAsDuplicateKeys = true; } {
-      globalSection = {
-        # https://github.com/mitchellh/ghostty/blob/main/src/config/Config.zig
+  programs.ghostty = lib.mkIf osConfig.glade.apps.enable {
+    enable = true;
 
-        theme = "catppuccin-${config.catppuccin.flavor}";
-        cursor-color =
-          lib.strings.removePrefix "#"
-            config.palette.${config.catppuccin.flavor}.colors.overlay1.hex;
+    # Ghostty package is broken on darwin.
+    package =
+      let
+        ghostty-mock = pkgs.writeShellScriptBin "ghostty-mock" ''
+          true
+        '';
+      in
+      ghostty-mock;
+    # Creates (invalid) symlink to file from the Ghostty package (which is mocked) in the store, bat fails to launch when it can't properly read the file.
+    installBatSyntax = false;
 
-        font-family = "Lilex Nerd Font";
-        font-size = 15;
-        adjust-cell-height = 10;
-        window-padding-x = 25;
-        window-padding-y = 25;
+    enableFishIntegration = true;
 
-        macos-titlebar-style = "tabs";
+    settings = {
+      theme = "catppuccin-${config.catppuccin.flavor}";
+      cursor-color =
+        lib.strings.removePrefix "#"
+          config.palette.${config.catppuccin.flavor}.colors.overlay1.hex;
 
-        working-directory = "${config.home.homeDirectory}/Dev";
-        window-inherit-working-directory = false;
+      font-family = "Lilex Nerd Font";
+      font-size = 15;
+      adjust-cell-height = 10;
+      window-padding-x = 25;
+      window-padding-y = 25;
 
-        copy-on-select = false;
-        shell-integration = "detect";
-        shell-integration-features = "no-cursor";
-        macos-option-as-alt = true;
+      macos-titlebar-style = "tabs";
 
-        keybind = [
-          "super+backspace=text:\x15"
-          "cmd+shift+d=new_split:down"
-          "cmd+shift+r=new_split:right"
-        ];
-      };
+      working-directory = "${config.home.homeDirectory}/Dev";
+      window-inherit-working-directory = false;
+
+      copy-on-select = false;
+      shell-integration = "detect";
+      shell-integration-features = "no-cursor";
+      macos-option-as-alt = true;
+
+      keybind = [
+        "super+backspace=text:\x15"
+        "cmd+shift+d=new_split:down"
+        "cmd+shift+r=new_split:right"
+      ];
     };
   };
 }
