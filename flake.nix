c171198b0597b48e9f7641917f4f070e36253727
uncenter <uncenter@uncenter.dev>
2025-02-25 14:18:15 -0500

    chore: remove extra input overrides for nixvim

--- flake.nix
+++ flake.nix
@@ -58,12 +58,6 @@
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-parts.follows = "flake-parts";
-        devshell.follows = "";
-        flake-compat.follows = "";
-        git-hooks.follows = "";
-        home-manager.follows = "";
-        nix-darwin.follows = "";
-        treefmt-nix.follows = "";
       };
     };
 
