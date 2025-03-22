546f41f40ab6d3b41a3a4f6422863e02a3ad3374
uncenter <uncenter@uncenter.dev>
2025-03-22 11:42:35 -0400

    fix(programs/eza): disable nushell integration

--- users/uncenter/programs/eza.nix
+++ users/uncenter/programs/eza.nix
@@ -2,6 +2,10 @@
 {
   programs.eza = lib.mkIf osConfig.glade.programs.enable {
     enable = true;
+
+    # Interferes with default ls command.
+    enableNushellIntegration = false;
+
     icons = "auto";
 
     extraOptions = [
