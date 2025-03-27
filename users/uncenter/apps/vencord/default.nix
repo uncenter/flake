9ea33356e1320fc541f0cdd86c9ac49951a7d26d
uncenter <uncenter@uncenter.dev>
2025-03-27 07:43:29 -0400

    feat(apps/vencord): switch to midnight discord with catppuccin variant

--- users/uncenter/apps/vencord/default.nix
+++ users/uncenter/apps/vencord/default.nix
@@ -18,8 +18,8 @@ in
     lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
       "${common}/settings.json".source = settingsFile;
       "${common}/quickCss.css".text = ''
-        @import url("https://catppuccin.github.io/discord/dist/catppuccin-${config.catppuccin.flavor}.theme.css") (prefers-color-scheme: dark);
-        @import url("https://catppuccin.github.io/discord/dist/catppuccin-latte.theme.css") (prefers-color-scheme: light);
+        @import url("https://raw.githubusercontent.com/uncenter/ctp-midnight/main/themes/${config.catppuccin.flavor}-${config.catppuccin.accent}.css") (prefers-color-scheme: dark);
+        @import url("https://raw.githubusercontent.com/uncenter/ctp-midnight/main/themes/latte-${config.catppuccin.accent}.css") (prefers-color-scheme: light);
       '';
     };
 }
