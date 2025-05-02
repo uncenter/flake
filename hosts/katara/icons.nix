4c56b3372513dd252faae96f75cb9159f820e4b4
uncenter <uncenter@uncenter.dev>
2025-05-01 22:24:22 -0400

    chore(hosts/katara): remove all custom icons except ghostty

--- hosts/katara/icons.nix
+++ hosts/katara/icons.nix
@@ -4,36 +4,8 @@
     enable = true;
     icons = [
       {
-        path = "/Applications/1Password.app";
-        icon = ./icons/1password.icns;
-      }
-      {
-        path = "/Applications/Calibre.app";
-        icon = ./icons/calibre.icns;
-      }
-      {
-        path = "/Applications/DevFolderIcon.app";
-        icon = ./icons/devfolder.icns;
-      }
-      {
         path = "/Applications/Ghostty.app";
         icon = ./icons/ghostty.icns;
-      }
-      {
-        path = "/Applications/Gramps.app";
-        icon = ./icons/gramps.icns;
-      }
-      {
-        path = "/Applications/ScreenshotsFolderIcon.app";
-        icon = ./icons/screenshotsfolder.icns;
-      }
-      {
-        path = "/Applications/Zed.app";
-        icon = ./icons/zed.icns;
-      }
-      {
-        path = "/Applications/zoom.us.app";
-        icon = ./icons/zoom.icns;
       }
     ];
   };
