ec898a06a57b7cd8ac73ac5e804d43e44e47b5d9
Ari Lotter <arilotter@gmail.com>
2025-05-03 12:14:33 -0400

    fix atf hash bug perhaps
    thank u isabelroses for reminding me to wrap it in nixpkgs.overlays

--- hosts/default.nix
+++ hosts/default.nix
@@ -11,6 +11,31 @@ let
   };
 
   normaliseClass = class: additionalClasses.${class} or class;
+
+  # for some reason, the hash of atf is ... wrong in nixpkgs?
+  # see https://github.com/NixOS/nixpkgs/issues/399767
+  # this overlay just uses the "correct" hash, verifiable by running
+  # `nix run nixpkgs#nix-prefetch-git -- https://github.com/freebsd/atf --rev atf-0.22 2>/dev/null | grep hash`
+  fixAtfHash =
+    { ... }:
+    {
+      nixpkgs.overlays = [
+        (final: prev: {
+          atf =
+            assert prev.lib.asserts.assertMsg (prev.atf.version == "0.22")
+              "ATF version changed - this hack is built for 0.22, but got ${prev.atf.version}. Consider removing this hack.";
+
+            prev.atf.overrideAttrs (oldAttrs: {
+              src = prev.fetchFromGitHub {
+                owner = "freebsd";
+                repo = "atf";
+                tag = "atf-0.22";
+                hash = "sha256-Uvi2/iZM1AfmBX+05fzNUlfSBSrGw0c3SE1qKyLa2Zo=";
+              };
+            });
+        })
+      ];
+    };
 in
 {
   imports = [ inputs.easy-hosts.flakeModule ];
@@ -43,6 +68,7 @@ in
             inputs.home-manager.darwinModules.home-manager
             inputs.tgirlpkgs.darwinModules.default
             inputs.darwin-custom-icons.darwinModules.default
+            fixAtfHash
           ])
 
           (lib.optionals (class == "wsl") [
