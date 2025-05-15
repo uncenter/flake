bde29d6488788f05c5a2cee680a6d3a2354c1b2d
uncenter <uncenter@uncenter.dev>
2025-05-15 13:28:49 -0400

    fix(tooling/rust): remove custom mold linker that never worked and is now broken

--- user/tooling/rust.nix
+++ user/tooling/rust.nix
@@ -1,23 +1,11 @@
 {
   lib,
   pkgs,
-  config,
   osConfig,
   ...
 }:
-let
-  toml = pkgs.formats.toml { };
-in
 {
   config = lib.mkIf osConfig.glade.tooling.rust.enable {
-    home.file."${config.xdg.dataHome}/cargo/config.toml".source = toml.generate "config.toml" {
-      linker = lib.getExe pkgs.clang;
-      rustflags = [
-        "-C"
-        "link-arg=-fuse-ld=${lib.getExe pkgs.mold}"
-      ];
-    };
-
     home.packages = with pkgs; [
       (rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; })
       rust-analyzer-unwrapped
