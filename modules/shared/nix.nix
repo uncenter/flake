a6fc0553deed27d9df1a3a544c05c54cb4f19ad7
uncenter <uncenter@uncenter.dev>
2025-05-24 13:29:30 -0400

    fix: add emscripten patches to enable tree-sitter webUISupport

--- modules/shared/nix.nix
+++ modules/shared/nix.nix
@@ -39,6 +39,21 @@
     overlays = [
       inputs.rust-overlay.overlays.default
       inputs.catppuccin-vsc.overlays.default
+      (final: prev: {
+        emscripten = prev.emscripten.overrideAttrs (attrs: {
+          patches = (attrs.patches or [ ]) ++ [
+            # These patches are needed to fix issues with overriding EM_CACHE, see https://github.com/NixOS/nixpkgs/issues/409511#issuecomment-2906258234.
+            (final.fetchpatch {
+              url = "https://github.com/emscripten-core/emscripten/commit/99c6e41154f701e423074e33a4fdaf5eea49d073.patch";
+              hash = "sha256-/wkhz08NhbgxsrXd7YFfdCGX6LrS2Ncct8dcwxBMsjY=";
+            })
+            (final.fetchpatch {
+              url = "https://github.com/emscripten-core/emscripten/commit/f4d358d740a238b67a1d6935e71638519d25afa0.patch";
+              hash = "sha256-hib5ZAN/R2dH+rTv3nYF37+xKZmeboKxnS+5mkht2lM=";
+            })
+          ];
+        });
+      })
     ];
 
     config = {
--- user/programs/default.nix
+++ user/programs/default.nix
@@ -106,9 +106,9 @@ in
         ito
         license-go
         star-history
-        # (tree-sitter.override {
-        #   webUISupport = true;
-        # })
+        (tree-sitter.override {
+          webUISupport = true;
+        })
         (wakatime-cli.overrideAttrs (_: {
           doCheck = false;
         }))
