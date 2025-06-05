98b076a7fa687d9dbbb89aba24b4f21313668efc
uncenter <uncenter@uncenter.dev>
2025-06-05 13:49:35 -0400

    feat(hosts/katara): vesktop, notion, obsidian, vscode, zed, & zen icons

--- hosts/katara/icons.nix
+++ hosts/katara/icons.nix
@@ -4,8 +4,32 @@
     enable = true;
     icons = [
       {
+        path = "/Applications/Vesktop.app";
+        icon = ./icons/discord.icns;
+      }
+      {
         path = "/Applications/Ghostty.app";
         icon = ./icons/ghostty.icns;
+      }
+      {
+        path = "/Applications/Notion.app";
+        icon = ./icons/notion.icns;
+      }
+      {
+        path = "/Applications/Obsidian.app";
+        icon = ./icons/obsidian.icns;
+      }
+      {
+        path = "/Applications/Visual Studio Code.app";
+        icon = ./icons/vscode.icns;
+      }
+      {
+        path = "/Applications/Zed.app";
+        icon = ./icons/zed.icns;
+      }
+      {
+        path = "/Applications/Zen.app";
+        icon = ./icons/zen.icns;
       }
     ];
   };
