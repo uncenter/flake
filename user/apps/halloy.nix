46005adae18bb647f2f05db91c6127c912b04b0a
uncenter <uncenter@uncenter.dev>
2025-08-04 19:05:37 -0400

    feat(apps/halloy): show topic banner, single pane view

--- user/apps/halloy.nix
+++ user/apps/halloy.nix
@@ -19,6 +19,23 @@
         nick_password_command = "${pkgs._1password-cli}/bin/op read op://Private/Libera.Chat/password";
         server = "irc.libera.chat";
       };
+
+      buffer.channel.topic.enabled = true; # show topic banner
+
+      # single pane: https://halloy.chat/guides/single-pane.html
+      actions = {
+        buffer = {
+          click_channel_name = "replace-pane";
+          click_highlight = "replace-pane";
+          click_username = "replace-pane";
+          local = "replace-pane";
+          message_channel = "replace-pane";
+          message_user = "replace-pane";
+        };
+        sidebar = {
+          buffer = "replace-pane";
+        };
+      };
     };
   };
 }
